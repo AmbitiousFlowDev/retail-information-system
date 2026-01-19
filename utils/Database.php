a12d874988ef8a4cd38a1da487df6730de195ab9
AmbitiousFlowDev <mohamed.lafrouh@gmail.com>
2026-01-19 20:50:28 +0100

    Add Client and Function model files; refactor Database class to Connection

--- utils/Database.php
+++ utils/Database.php
@@ -1,6 +1,15 @@
 <?php
 
 
-class Database extends PDO {
-    // Database connection parameters
+
+class Connection extends PDO {
+    private $host     = DB_SERVER;
+    private $username = DB_USERNAME;
+    private $password = DB_PASSWORD;
+    private $database = DB_NAME;
+
+    public function __construct() {
+        $dsn = "mysql:host=$this->host;dbname=$this->database;charset=utf8mb4";
+        parent::__construct($dsn, $this->username, $this->password);
+    }
 }
